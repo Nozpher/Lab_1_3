bd8d26f012d628f14379cf999c410e679d4fa37b
Nozpher <66372864+Nozpher@users.noreply.github.com>
2022-08-08 16:46:09 +0500

    Добавьте файлы проекта.

---
+++ Lab_1_3/Lab_1_3.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+
+using namespace std;
+
+
+
+
+int main()
+{
+	float n, m, a, b, c;
+	cin >> n;
+	cin >> m;
+	a = ++n * ++m;
+	cout << a;
+
+	cin >> n;
+	cin >> m;
+	b = m ++ < n;
+	cout << b;
+
+	cin >> n;
+	cin >> m;
+	c = n++ > m;
+	cout << c;
+
+}
